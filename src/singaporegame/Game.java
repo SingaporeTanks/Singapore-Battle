07e2113b8922d38b37235e5b90e5e7bc0143c4a8
Evgeni Viyachev <evgeny.viyachev@gmail.com>
2016-04-10 11:22:12 +0300

    added safety features
    checks if the game is already running or has stopped

--- src/singaporegame/Game.java
+++ src/singaporegame/Game.java
@@ -8,86 +8,85 @@ import java.awt.image.ImageObserver;
 import java.io.File;
 
 public class Game extends Canvas implements Runnable {
-    public static final int WIDTH=800, HEIGHT=600;
+    public static final int WIDTH = 800, HEIGHT = 600;
     private Thread thread;
-    boolean running=false;
-    private BufferedImage singaporebkg=null;
+    boolean running = false;
+    private BufferedImage singaporebkg = null;
 
-    public Game(){
-
-        new Window(WIDTH, HEIGHT,"Singapore Battle", this);
+    public Game() {
+        new Window(WIDTH, HEIGHT, "Singapore Battle", this);
     }
-    public synchronized void start(){
-        thread=new Thread(this);
+
+    public synchronized void start() {
+        //checking if the game is already running
+        if (running) {
+            return;
+        }
+        thread = new Thread(this);
         thread.start();
-        running=true;
-
+        running = true;
     }
-    public void run() {
 
+    public void run() {
         this.requestFocus();
-        long lastTIme=System.nanoTime();
-        double amountOfTicks=60;
-        double ns=1000000000/amountOfTicks;
-        double delta=0;
-        long timer=System.currentTimeMillis();
-        int frames=0;
-        while(running){
-            long now=System.nanoTime();
-            delta+=(now-lastTIme)/ns;
-            lastTIme=now;
-            while(delta>=1) {
+        long lastTIme = System.nanoTime();
+        double amountOfTicks = 60;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTIme) / ns;
+            lastTIme = now;
+            while (delta >= 1) {
                 refresh();
                 delta--;
             }
-            if(running)
+            if (running) {
                 render();
-            frames++;
-            if(System.currentTimeMillis()-timer>1000){
-                timer+=1000;
+                frames++;
+            }
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
                 // System.out.println("FPS:"+frames);
-                frames=0;
+                frames = 0;
             }
         }
         stop();
+    }
 
+    private void refresh() {
 
     }
 
-    private void refresh(){
-
-    }
-    private void render(){
-        BufferStrategy bs=this.getBufferStrategy();
-        if(bs==null){
+    private void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
             this.createBufferStrategy(3);
             return;
         }
-        Graphics g=bs.getDrawGraphics();
+        Graphics g = bs.getDrawGraphics();
         g.setColor(Color.black);
-        g.fillRect(0,0,WIDTH,HEIGHT);
-
+        g.fillRect(0, 0, WIDTH, HEIGHT);
         g.dispose();
         bs.show();
-
     }
 
-    public void stop(){
-        try{
-            thread.join();
-            running=false;
+    public void stop() {
+        //checking if the game has already stopped
+        if (!running) {
+            return;
         }
-        catch(Exception e){
+        running = false;
+        try {
+            thread.join();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-	new Game();
-
-
+        new Game();
     }
-
-
-
 }
